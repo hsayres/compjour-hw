81370fa2c2b8193abb439d937cd5ed21e100667d
hsayres <hsayres@stanford.edu>
2015-04-07 17:38:44 -0700

    added files
    hello-world
    imports-and-setup
    stanford-news-download

---
+++ opener-project/hello-world.py
@@ -0,0 +1,7 @@
+print("Hello world")
+myname = "Haley"
+print("My name is", myname)
+myage=100
+print("I am", myage, "years old")
+print("That is roughly", 100*365, "days")
+print("Or,", 100*365*24*60*60, "seconds")
