f5dd912172b7ba2c9997e4c769aca26ace16910b
hsayres <hsayres@stanford.edu>
2015-04-26 11:58:59 -0700

    started skeleton

---
+++ twitterbot/bot.py
@@ -0,0 +1,8 @@
+import foo
+import twit_utils
+CREDS_FILE = "~/Desktop/tw.json"
+
+def make_it_so():
+"""
+
+"""
---
+++ twitterbot/foo.py
@@ -0,0 +1,34 @@
+def find_first_feedme_tagged_mention(mentions):
+	"""
+    Given a list of tweets (ostensibly from the mentions-API-endpoint),
+    find the earliest one that has the #FEEDME hashtag in it
+    
+    Arguments:
+        mentions (list): a list of Twitter tweet objects that are dicts
+    
+    Returns:
+        if any such tweet is found, return that tweet (dict)
+        else, return None
+    """
+
+def latest_feedme_reply(tweets):
+    """
+    Given a list of tweets (ostensibly from user_timeline API endpoint),
+    find the earliest one that has the #FEEDME hashtag in it
+    
+    Arguments:
+        tweets (list): a list of Twitter tweet objects that are dicts
+    
+    Returns:
+        if any such tweet is found, return that tweet (dict)
+        else, return None
+    """
+
+def make_feedme_text():
+	"""
+    Create a custom #FEEDME  message
+    Arguments:
+        None
+    Returns:
+        A text string with the #FEEDME hashtag and something extra special
+    """
