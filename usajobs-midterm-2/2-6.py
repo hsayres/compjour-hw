19dabebdba1698db319ad3c001f02ae34fb53b32
hsayres <hsayres@stanford.edu>
2015-05-07 15:01:29 -0700

    2-6.py
    finished

--- usajobs-midterm-2/2-6.py
+++ usajobs-midterm-2/2-6.py
@@ -6,12 +6,16 @@ with open("data-hold/intl-jobcount.json") as f:
     intl_data = json.loads(f.read())
 
 mylist=[]
+total=0
 for d in intl_data:
 	if d[1] > 10:
 		mylist.append([d[0], d[1]])
+	else:
+		total += d[1]
 sortedlist = sorted(mylist, key=keydef, reverse=True)
 for item in sortedlist:
 	namestring = item[0]+","+str(item[1])
 	print(namestring)
+print("Other:", total)
 
 
